b89e3d419c2370434eed906687b979f7a00b168b
AdeDom <pathiphonyw@gmail.com>
2020-01-25 02:48:40 +0700

    Multi player

--- app/src/main/java/com/adedom/theegggame/ui/multi/multi/Item.kt
+++ app/src/main/java/com/adedom/theegggame/ui/multi/multi/Item.kt
@@ -7,13 +7,14 @@ import com.adedom.theegggame.R
 import com.adedom.theegggame.data.models.Multi
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 
-class Item(multiple: ArrayList<Multi>) {
+class Item(multiple: ArrayList<Multi>, markerItems: ArrayList<Marker>) {
 
     init {
-        MultiActivityViewModel.markerItems.removeMarkers()
+        markerItems.removeMarkers()
         multiple.forEach {
-            MultiActivityViewModel.markerItems.setMarkers(
+            markerItems.setMarkers(
                 GoogleMapActivity.sGoogleMap,
                 LatLng(it.latitude, it.longitude),
                 BitmapDescriptorFactory.fromResource(R.drawable.the_egg_game)
