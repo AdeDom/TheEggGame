cb15899264bc3d879e398bdc23a0ee577b48333a
AdeDom <pathiphonyw@gmail.com>
2020-02-01 23:23:57 +0700

    new base url

--- app/src/main/java/com/adedom/theegggame/data/BaseUrl.kt
+++ app/src/main/java/com/adedom/theegggame/data/BaseUrl.kt
@@ -1,10 +1,10 @@
 package com.adedom.theegggame.data
 
-private val HOST = "192.168.43.22"
-val BASE_URL = "http://$HOST/the-egg-game/api/"
+private const val HOST = "192.168.43.22"
+const val BASE_URL = "http://$HOST/the-egg-game/api/"
 
 //const val BASE_URL = "http://pathiphon.000webhostapp.com/api/"
 
 //const val BASE_URL = "http://tm.dru.ac.th/apidb/upload/api/"
 
-fun imageUrl(image: String) = "$BASE_URL../profiles/$image"
+fun imageUrl(image: String) = "${BASE_URL}profiles/$image"
