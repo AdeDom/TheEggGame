ff01b21aec8b9a1496a72ff8b615ebbb7e3fab2f
AdeDom <pathiphonyw@gmail.com>
2020-02-01 23:23:20 +0700

    Companion object player

--- app/src/main/java/com/adedom/theegggame/data/models/Player.kt
+++ app/src/main/java/com/adedom/theegggame/data/models/Player.kt
@@ -1,7 +1,5 @@
 package com.adedom.theegggame.data.models
 
-import android.os.Parcel
-import android.os.Parcelable
 import com.adedom.library.util.*
 import com.google.gson.annotations.SerializedName
 
@@ -13,38 +11,4 @@ data class Player(
     @SerializedName(KEY_VALUES5) val level: Int? = null,
     @SerializedName(KEY_VALUES6) val state: String? = null,
     @SerializedName(KEY_VALUES7) val gender: String? = null
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readString(),
-        parcel.readString()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(playerId)
-        parcel.writeString(username)
-        parcel.writeString(name)
-        parcel.writeString(image)
-        parcel.writeValue(level)
-        parcel.writeString(state)
-        parcel.writeString(gender)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Player> {
-        override fun createFromParcel(parcel: Parcel): Player {
-            return Player(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Player?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+)
