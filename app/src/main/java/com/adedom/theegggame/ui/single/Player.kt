ddaa6a035917c830d99d01b1b3a961c48f079526
AdeDom <pathiphonyw@gmail.com>
2020-02-02 01:53:04 +0700

    Single -> player

--- app/src/main/java/com/adedom/theegggame/ui/single/Player.kt
+++ app/src/main/java/com/adedom/theegggame/ui/single/Player.kt
@@ -1,60 +1,58 @@
 package com.adedom.theegggame.ui.single
 
-import android.content.Context
 import com.adedom.library.extension.*
+import com.adedom.library.util.GoogleMapActivity.Companion.sContext
+import com.adedom.library.util.GoogleMapActivity.Companion.sGoogleMap
+import com.adedom.library.util.GoogleMapActivity.Companion.sLatLng
 import com.adedom.theegggame.R
 import com.adedom.theegggame.data.imageUrl
 import com.adedom.theegggame.ui.main.MainActivity
 import com.adedom.theegggame.util.CIRCLE_ONE_HUNDRED_METER
 import com.adedom.theegggame.util.getLevel
 import com.adedom.theegggame.util.setImageProfile
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
 
-class Player(private val context: Context, private val googleMap: GoogleMap?, latLng: LatLng) {
+class Player {
 
     init {
+        //remove
         myMarker?.removeMarker()
         myCircle?.removeCircle()
 
-        setMyLocation(latLng)
-        googleMap!!.setCircle(latLng, CIRCLE_ONE_HUNDRED_METER)
-    }
-
-    private fun setMyLocation(latLng: LatLng) {
+        //set
         val player = MainActivity.sPlayer
         setImageProfile(player.image, player.gender, {
-            googleMap!!.setMarker(
-                latLng,
+            sGoogleMap!!.setMarker(
+                sLatLng,
                 BitmapDescriptorFactory.fromResource(R.drawable.ic_player),
                 player.name!!,
                 getLevel(player.level)
             )
         }, {
-            googleMap!!.setMarker(
-                latLng,
+            sGoogleMap!!.setMarker(
+                sLatLng,
                 BitmapDescriptorFactory.fromResource(R.drawable.ic_player_female),
                 player.name!!,
                 getLevel(player.level)
             )
         }, {
-            context.loadBitmap(imageUrl(player.image!!), {
-                googleMap!!.setMarker(
-                    latLng,
+            sContext.loadBitmap(imageUrl(player.image!!), {
+                sGoogleMap!!.setMarker(
+                    sLatLng,
                     BitmapDescriptorFactory.fromBitmap(it),
                     player.name!!,
                     getLevel(player.level)
                 )
             }, {
-                googleMap!!.setMarker(
-                    latLng,
+                sGoogleMap!!.setMarker(
+                    sLatLng,
                     BitmapDescriptorFactory.fromResource(R.drawable.ic_player),
                     player.name!!,
                     getLevel(player.level)
                 )
             })
         })
+        sGoogleMap!!.setCircle(sLatLng, CIRCLE_ONE_HUNDRED_METER)
     }
 
 }
