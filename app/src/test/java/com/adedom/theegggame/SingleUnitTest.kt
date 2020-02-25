eb2b29ca94389673b56090c8af19ca10e93aaf12
AdeDom <pathiphonyw@gmail.com>
2020-02-25 18:34:43 +0700

    test

--- app/src/test/java/com/adedom/theegggame/SingleUnitTest.kt
+++ app/src/test/java/com/adedom/theegggame/SingleUnitTest.kt
@@ -2,25 +2,9 @@ package com.adedom.theegggame
 
 class SingleUnitTest {
 
-//    val single = SingleActivityViewModel()
-//
 //    @Test
-//    fun testTitleItem() {
-//        val title = single.titleItem(4)
+//    fun singleUnitTest() {
 //
-//        println(title)
-//    }
-//
-//    @Test
-//    fun testDetailItem() {
-//        val detail = single.detailItem(5, 0)
-//
-//        println(detail)
-//    }
-//
-//    @Test
-//    fun testKeepItemSingle() {
-//        single.keepItemSingle("1", 2, 3, 4.5, 6.7)
 //    }
 
 }
