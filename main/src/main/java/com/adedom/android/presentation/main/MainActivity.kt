f05974303cef17ee1fd868e7fff4c26d36dc0b7c
AdeDom <pathiphonyw@gmail.com>
2020-10-20 22:07:11 +0700

    replace screen main from module app to module main

--- main/src/main/java/com/adedom/android/presentation/main/MainActivity.kt
+++ main/src/main/java/com/adedom/android/presentation/main/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.adedom.android.presentation.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.adedom.android.R
+import com.adedom.android.util.toast
+import com.adedom.teg.presentation.main.MainViewModel
+import com.adedom.teg.util.TegConstant
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModel<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel.error.observe(this, {
+            it.throwable.printStackTrace()
+            toast(it.throwable.message)
+        })
     }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.callPlayerState(TegConstant.STATE_ONLINE)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viewModel.callPlayerState(TegConstant.STATE_OFFLINE)
+    }
+
 }
--- main/src/main/java/com/adedom/android/presentation/main/MainFragment.kt
+++ main/src/main/java/com/adedom/android/presentation/main/MainFragment.kt
@@ -2,11 +2,10 @@ package com.adedom.android.presentation.main
 
 import android.os.Bundle
 import android.view.View
-import androidx.navigation.fragment.findNavController
 import com.adedom.android.R
 import com.adedom.android.base.BaseFragment
+import com.adedom.android.util.setImageCircle
 import com.adedom.teg.presentation.main.MainViewModel
-import com.adedom.teg.util.TegConstant
 import kotlinx.android.synthetic.main.fragment_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -24,40 +23,31 @@ class MainFragment : BaseFragment(R.layout.fragment_main) {
         viewModel.playerInfo.observe(viewLifecycleOwner, { playerInfo ->
             if (playerInfo == null) return@observe
 
-            val text = "${playerInfo.name} : ${playerInfo.level}"
-            materialTextView.text = text
+            ivImageProfile.setImageCircle(playerInfo.image)
+            tvName.text = playerInfo.name
+            tvLevel.text = getString(R.string.level, playerInfo.level)
         })
 
         viewModel.error.observeError()
 
         viewModel.fetchPlayerInfo()
 
-        btSignOut.setOnClickListener {
-            viewModel.signOut()
-            findNavController().navigate(R.id.action_global_splashScreenFragment)
-        }
-
-        btChangePassword.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_changePasswordFragment)
-        }
-
-        btChangeProfile.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_changeProfileFragment)
-        }
-
-        btChangeImageProfile.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_changeImageFragment)
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        viewModel.callPlayerState(TegConstant.STATE_ONLINE)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        viewModel.callPlayerState(TegConstant.STATE_OFFLINE)
+//        btSignOut.setOnClickListener {
+//            viewModel.signOut()
+//            findNavController().navigate(R.id.action_global_splashScreenFragment)
+//        }
+//
+//        btChangePassword.setOnClickListener {
+//            findNavController().navigate(R.id.action_mainFragment_to_changePasswordFragment)
+//        }
+//
+//        btChangeProfile.setOnClickListener {
+//            findNavController().navigate(R.id.action_mainFragment_to_changeProfileFragment)
+//        }
+//
+//        btChangeImageProfile.setOnClickListener {
+//            findNavController().navigate(R.id.action_mainFragment_to_changeImageFragment)
+//        }
     }
 
 }
