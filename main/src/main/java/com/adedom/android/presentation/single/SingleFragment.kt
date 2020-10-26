549fd84a37555eb78c4c850f4bd6e373634e51c9
AdeDom <pathiphonyw@gmail.com>
2020-10-26 21:18:09 +0700

    refactor code camera get location one time and Map getMapAsync

--- main/src/main/java/com/adedom/android/presentation/single/SingleFragment.kt
+++ main/src/main/java/com/adedom/android/presentation/single/SingleFragment.kt
@@ -10,26 +10,27 @@ import androidx.lifecycle.asLiveData
 import androidx.navigation.fragment.findNavController
 import com.adedom.android.R
 import com.adedom.android.base.BaseFragment
+import com.adedom.android.util.awaitLastLocation
 import com.adedom.android.util.locationFlow
 import com.adedom.android.util.setVisibility
 import com.adedom.android.util.toast
 import com.adedom.teg.presentation.single.SingleViewModel
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.fragment_single.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 @ExperimentalCoroutinesApi
-class SingleFragment : BaseFragment(R.layout.fragment_single), OnMapReadyCallback {
+class SingleFragment : BaseFragment(R.layout.fragment_single) {
 
     private val viewModel by viewModel<SingleViewModel>()
-    private var mGoogleMap: GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,11 +38,26 @@ class SingleFragment : BaseFragment(R.layout.fragment_single), OnMapReadyCallbac
         activity?.window?.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 
+    @SuppressLint("MissingPermission")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val locationProviderClient = LocationServices
+            .getFusedLocationProviderClient(requireActivity())
+
         mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync(this)
+        mapView.getMapAsync { googleMap ->
+            googleMap.isMyLocationEnabled = true
+            googleMap.setMinZoomPreference(12F)
+            googleMap.setMaxZoomPreference(16F)
+
+            CoroutineScope(Dispatchers.Main).launch {
+                val location = locationProviderClient.awaitLastLocation()
+                val latLng = LatLng(location.latitude, location.longitude)
+                val cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 14F)
+                googleMap?.animateCamera(cameraUpdate)
+            }
+        }
 
         viewModel.state.observe { state ->
             progressBar.setVisibility(state.loading)
@@ -53,7 +69,7 @@ class SingleFragment : BaseFragment(R.layout.fragment_single), OnMapReadyCallbac
             viewModel.callItemCollection()
         }
 
-        LocationServices.getFusedLocationProviderClient(requireActivity())
+        locationProviderClient
             .locationFlow()
             .conflate()
             .catch { e ->
@@ -69,18 +85,9 @@ class SingleFragment : BaseFragment(R.layout.fragment_single), OnMapReadyCallbac
         }
     }
 
-    @SuppressLint("MissingPermission")
-    override fun onMapReady(googleMap: GoogleMap?) {
-        mGoogleMap = googleMap
-        mGoogleMap?.isMyLocationEnabled = true
-        mGoogleMap?.setMinZoomPreference(12F)
-        mGoogleMap?.setMaxZoomPreference(16F)
-    }
-
     private fun onLocationResult(location: Location) {
         val latLng = LatLng(location.latitude, location.longitude)
-        val cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 14F)
-        mGoogleMap?.animateCamera(cameraUpdate)
+        context.toast("${latLng.latitude}, ${latLng.longitude}")
     }
 
     override fun onResume() {
