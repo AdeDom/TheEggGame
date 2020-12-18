e2e8df4da9a892088fa191b595756bf187f0d19f
AdeDom <pathiphonyw@gmail.com>
2020-12-19 02:16:57 +0700

    delete module app & admin also keep file raw sound & music

--- admin/src/main/java/com/adedom/admin/util/BaseViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.adedom.admin.util
-
-import androidx.lifecycle.ViewModel
-import com.adedom.admin.data.network.BaseApi
-import com.adedom.admin.data.repositories.BaseRepository
-
-abstract class BaseViewModel : ViewModel() {
-    val repository by lazy { BaseRepository(BaseApi.invoke()) }
-}
